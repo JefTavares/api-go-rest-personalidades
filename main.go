ee46b0eb9075ad4333e0ce26cd6a3740c30c22cc
Jeferson Tavares <jefersontavaressilva@gmail.com>
2022-11-21 22:39:33 -0300

    Adicionar dependencia gorilla/mux + metodo RetornaUmaPersonalidade

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	models.Personalidades = []models.Personalidade{
-		{Nome: "nome 1", Historia: "historia 1"},
-		{Nome: "nome 2", Historia: "historia 2"},
+		{Id: 1, Nome: "nome 1", Historia: "historia 1"},
+		{Id: 2, Nome: "nome 2", Historia: "historia 2"},
 	}
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandlerRequest()
--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jeftavares/api-go-rest/controllers"
 )
 
 // lidar com as requisições (tradução livre de handler request ;)
 func HandlerRequest() {
-	http.HandleFunc("/", controllers.Home) //cria a rota
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil)) //log caso de algum erro e sobe o servidor
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home) //cria a rota
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods(http.MethodGet)
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(":8000", r)) //log caso de algum erro e sobe o servidor
 }
