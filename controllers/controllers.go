6cbcb4c1b1b56b01de1f1916f370fe5e84d577da
Jeferson Tavares <jefersontavaressilva@gmail.com>
2022-11-22 23:21:18 -0300

    implmentação do gorm e drive do postgres

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/jeftavares/api-go-rest/database"
 	"github.com/jeftavares/api-go-rest/models"
 )
 
@@ -15,16 +15,17 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	var p []models.Personalidade
+	database.DB.Find(&p) //Passa o endereço de memoria no .Find (&p)
+	json.NewEncoder(w).Encode(p)
 }
 
 func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	for _, personalidade := range models.Personalidades {
-		if strconv.Itoa(personalidade.Id) == id {
-			json.NewEncoder(w).Encode(personalidade)
-		}
-	}
+	var p models.Personalidade
+	database.DB.First(&p, id)
+	json.NewEncoder(w).Encode(p)
+
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/jeftavares/api-go-rest/database"
 	"github.com/jeftavares/api-go-rest/models"
 	"github.com/jeftavares/api-go-rest/routes"
 )
@@ -12,6 +13,9 @@ func main() {
 		{Id: 1, Nome: "nome 1", Historia: "historia 1"},
 		{Id: 2, Nome: "nome 2", Historia: "historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
+
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandlerRequest()
 }
